3ac7613bda854f2e86b0000362708493925b9753
Steve Harris <doc@steveharris.me>
2021-12-21 10:56:10 +0000

    wip: deploy to gh-pages

--- uclhal/uclhal/conf.py
+++ uclhal/uclhal/conf.py
@@ -670,12 +670,22 @@ REDIRECTIONS = []
 #     ]
 # }
 
+# Run this ./uclhal/uclhal
+DEPLOY_COMMANDS = {
+    'default': [
+        "ghp-import --no-jekyll --push --message='deploying output'  output",
+    ]
+}
+
+# ^^^ SEE ABOVE: NOT using the default github_deploy command b/c of nested directory
+# ----------------------------------------------------------------------------------
+
 # github_deploy configuration
 # For more details, read the manual:
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
 GITHUB_SOURCE_BRANCH = 'src'
-GITHUB_DEPLOY_BRANCH = 'master'
+GITHUB_DEPLOY_BRANCH = 'gh-pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
 GITHUB_REMOTE_NAME = 'origin'
